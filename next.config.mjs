2fe77729d8b41f6d41d11c13d7660d64bd2980b5
AbdulKhaliq59 <kananuraabdulkhaliq59@gmail.com>
2024-12-19 14:17:50 +0200

    bug: fix mjs configuration structure

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: ['deploy-preview-7--qcode-staging.netlify.app']
+    images:
+    {
+        domains: ['deploy-preview-7--qcode-staging.netlify.app'],
+
+    }
 };
 
 export default nextConfig;
