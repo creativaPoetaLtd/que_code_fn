393c1e47e7c11361b0fc7a499f2a82236137ef0f
abdulkhaliq59 <kananuraabdulkhaliq59@gmail.com>
2026-01-12 18:49:43 +0200

    feat: adding real sw.js import

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,1 @@
-<!-- PushAlert Unified Code -->
-<script type="text/javascript">
-    (function(d, t) {
-        var g = d.createElement(t),
-        s = d.getElementsByTagName(t)[0];
-        g.src = "https://cdn.pushalert.co/unified_0fe68d0326f325e63c546b3d13037915.js";
-        s.parentNode.insertBefore(g, s);
-    }(document, "script"));
-</script>
-<!-- End PushAlert Unified Code -->
+importScripts("https://cdn.pushalert.co/sw-87118_2.js");
