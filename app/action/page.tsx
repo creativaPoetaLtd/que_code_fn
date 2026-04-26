b1d442431743820a5428a58f06207432a4302e27
Igor <iigore2061@gmail.com>
2026-04-26 19:58:03 +0200

    feat: enhance page component with Suspense for ActionPageLayout

--- app/action/page.tsx
+++ app/action/page.tsx
@@ -2,7 +2,7 @@
 import ActionPageLayout from '@/components/ActionPage/ActionPageLayout'
 import { Header } from '@/components/Header'
 import Navigation from '@/components/Navigation'
-import React from 'react'
+import React, { Suspense } from 'react'
 import { useSidebar } from '@/context/SidebarContext'
 import { cn } from '@/lib/utils'
 
@@ -10,24 +10,20 @@ const page = () => {
     const { isExpanded } = useSidebar();
     return (
         <div className="flex min-h-screen bg-gray-50">
-            {/* Desktop Sidebar */}
             <Navigation />
 
-            {/* Main Content */}
             <main className={cn(
                 "flex-1 flex flex-col p-8 transition-all duration-300",
                 isExpanded ? "lg:ml-64" : "lg:ml-20"
             )}>
                 <div className="flex-1 overflow-y-auto pb-24 lg:pb-8">
-                    {/* Header */}
                     <Header />
-
-                    {/* Content */}
-                    <ActionPageLayout />
+                    <Suspense>
+                        <ActionPageLayout />
+                    </Suspense>
                 </div>
             </main>
 
-            {/* Bottom Navigation for small devices */}
             <div className="lg:hidden">
                 <Navigation />
             </div>
